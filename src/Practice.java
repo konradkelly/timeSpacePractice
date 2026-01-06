9bdc8bc7a41e644eacf319f0c8c25070d80835a6
Jonus <joclapshaw@gmail.com>
2026-01-06 14:43:25 -0800

    Problem 2

--- src/Practice.java
+++ src/Practice.java
@@ -17,8 +17,8 @@ public class Practice {
     return evens;
   }
 
-  // Time Complexity:
-  // Space Complexity:
+  // Time Complexity: O(n)
+  // Space Complexity: O(1)
   public static int sumDiagonal(int[][] matrix) {
     int sum = 0;
     for (int i = 0; i < matrix.length; i++) {
