51d499b112e248471db08e7ddfd7436e138dd09d
konradkelly <konradky@gmail.com>
2026-01-06 15:00:16 -0800

    implement countFrequencies method

--- src/Practice.java
+++ src/Practice.java
@@ -28,8 +28,8 @@ public class Practice {
   }
   
 
-  // Time Complexity: 
-  // Space Complexity: 
+  // Time Complexity: O(n)
+  // Space Complexity: O(n)
   // Does the 'T' look confusing? Consider refreshing on generic methods
   // We'll revisit generics as a class later
   public static <T> Map<T, Integer> countFrequencies(T[] array) {
