38322ed2b28e8c31bd943af45b22a1d762a3763b
konradkelly <konradky@gmail.com>
2026-01-07 11:57:05 -0800

    test: add ordered and unordered array tests for mostCommonSpaceEfficient

--- src/PracticeTest.java
+++ src/PracticeTest.java
@@ -8,4 +8,18 @@ public class PracticeTest {
 
   // Hints: They are static methods, so you will use the full Practice.mostCommonTimeEfficient for method calls
 
+
+  @Test
+  public void testMostCommonSpaceEfficient_whenOrderedArray() {
+    int[] nums = {97, 98, 99, 99, 99, 100};
+    int actual = Practice.mostCommonSpaceEfficient(nums);
+    assertEquals(99, actual);
+  }
+
+  @Test
+  public void testMostCommonSpaceEfficient_whenUnorderedArray() {
+    int[] nums = {100, 99, 98, 99, 97, 99};
+    int actual = Practice.mostCommonSpaceEfficient(nums);
+    assertEquals(99, actual);
+  }
 }
