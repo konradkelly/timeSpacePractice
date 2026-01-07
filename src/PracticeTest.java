ca3839b6fdcbd90634b99b0a3b756ee5c6773e02
konradkelly <konradky@gmail.com>
2026-01-07 13:07:41 -0800

    add unit tests for countFrequencies method

--- src/PracticeTest.java
+++ src/PracticeTest.java
@@ -1,12 +1,40 @@
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.Map;
+
 
 public class PracticeTest {
   
   // TODO: Implement tests for Practice.mostCommonTimeEfficient and Practice.mostCommonSpaceEfficient
 
   // Hints: They are static methods, so you will use the full Practice.mostCommonTimeEfficient for method calls
+  @Test
+  public void testCountFrequencies_whenIntegersHaveDifferentFrequencies() {
+    Integer[] nums = {97, 97, 97, 97, 98, 98, 99, 100, 100, 100};
+    Map<Integer, Integer> actual = Practice.countFrequencies(nums);
+    
+    assertEquals(4, actual.get(97));
+    assertEquals(2, actual.get(98));
+    assertEquals(1, actual.get(99));
+    assertEquals(3, actual.get(100));
+  }
+
+  @Test
+  public void testCountFrequencies_whenOnlyTheSameInteger() {
+    Integer[] nums = {94, 94, 94, 94, 94};
+    Map<Integer, Integer> actual = Practice.countFrequencies(nums);
+    
+    assertEquals(5, actual.get(94));
+  }
+
+  @Test
+  public void testCountFrequencies_whenArrayIsEmpty() {
+    Integer[] nums = {};
+    Map<Integer, Integer> actual = Practice.countFrequencies(nums);
+    
+    assertTrue(actual.isEmpty());
+  }
 
   @Test
   public void testMostCommonTimeEfficient_whenOrderedArray() {
